24b0e1e16decd3ef4054d33fd43ab0b29282684a
A-Wu5 <roboanthony@gmail.com>
2024-07-01 18:56:35 -0700

    Added new post feature and success pages on new post submit

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
 
+var blogPosts = {};
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/new-post", (req, res) => {
+  let postTitle = req.body["title"];
+  if (postTitle in blogPosts) {
+    //Handle duplicate titles
+  } else {
+    // insert new post object into blogPosts
+    blogPosts[postTitle] = {
+      title: postTitle,
+      author_first: req.body["first-name"],
+      author_last: req.body["last-name"],
+      date: new Date(),
+      content: req.body["content"],
+    };
+    res.render("submitted.ejs");
+  }
+});
+
+app.post("/edit-post", (req, res) => {
+  let postTitle = req.body["title"];
+  blogPosts[postTitle][content] = req.body["content"];
+  res.render("submitted.ejs");
+});
+
+app.get("/view/:title", (req, res) => {
+  let title = req.params.title;
+  res.render(`/view/${title}`, { post: blogPosts[title] });
+});
+
+app.get("/new-post", (req, res) => {
+  res.render("new-post.ejs");
+});
 
 app.get("/", (req, res) => {
-  res.render("homepage.ejs");
+  res.render("homepage.ejs", { posts: blogPosts });
 });
 
 app.listen(PORT, () => {
